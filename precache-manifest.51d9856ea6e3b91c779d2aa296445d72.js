e73e0e57c7824218c428c85151e3f4e93f233ebf
sario-tedesco <sariotedesco@gmail.com>
2022-05-30 23:50:37 +0200

    Updates

---
+++ precache-manifest.51d9856ea6e3b91c779d2aa296445d72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b81ee1f0560e7e41674a",
+    "url": "/portfolio/static/css/main.96935ca8.chunk.css"
+  },
+  {
+    "revision": "b81ee1f0560e7e41674a",
+    "url": "/portfolio/static/js/main.b81ee1f0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/portfolio/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/portfolio/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "4e79aa1fef427f3be1c9",
+    "url": "/portfolio/static/js/runtime~main.4e79aa1f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "265c44cf892931120d26bedf78777c7c",
+    "url": "/portfolio/index.html"
+  }
+];
