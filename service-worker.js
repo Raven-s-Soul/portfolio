c8ee00d7f3432161109e4aaa952b31366f1b9df7
sario-tedesco <55527565+sario-tedesco@users.noreply.github.com>
2022-05-31 21:44:02 +0000

    Deploying to gh-pages from @ sario-tedesco/portfolio@f039c38a4cfa6385bc7ff4dad15fb4fb5751e7e0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8459ed1ea708ff90c29be706061e1017.js"
+  "/portfolio/precache-manifest.c6dc6461cbcba9f69ea6a243af22d9d5.js"
 );
 
 workbox.clientsClaim();
