a13aa34a640dd3b326a11128b6d87159f661d64b
sario-tedesco <55527565+sario-tedesco@users.noreply.github.com>
2022-06-01 01:16:33 +0000

    Deploying to gh-pages from @ sario-tedesco/portfolio@5a5836e6b3d3bac094cc4b84b18f5c9ae222e1fd 🚀

---
+++ precache-manifest.8ec9d5bcfc23a3f70bad0acb9f1bc7b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0edd65d4a34cf4e4cb31",
+    "url": "/portfolio/static/css/main.5592cbba.chunk.css"
+  },
+  {
+    "revision": "0edd65d4a34cf4e4cb31",
+    "url": "/portfolio/static/js/main.0edd65d4.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/portfolio/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "1d578078c479fdd46904",
+    "url": "/portfolio/static/js/2.1d578078.chunk.js"
+  },
+  {
+    "revision": "8b6aa0a95ca45fd3a7a1",
+    "url": "/portfolio/static/js/runtime~main.8b6aa0a9.js"
+  },
+  {
+    "revision": "815f2359ecd2f342ced88aad7d3d848a",
+    "url": "/portfolio/static/media/PictureAbout.815f2359.png"
+  },
+  {
+    "revision": "12822e608cf9e8b83894473581e6643f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.27364ec9a07cc5b1da0ee62811ad99e3.js"
+  "/portfolio/precache-manifest.8ec9d5bcfc23a3f70bad0acb9f1bc7b1.js"
 );
 
 workbox.clientsClaim();
