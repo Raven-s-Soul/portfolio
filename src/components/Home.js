39fcdceb4a1b2bb9e7094aece04a324ff66a5160
sario-tedesco <sariotedesco@gmail.com>
2022-06-05 19:47:53 +0200

    update -cant fix reactive navbar yet

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,12 @@ function Home() {
         <br/>
         <div className='container'>
             <div className='left'>
-                <img src={Picture}  width="50%"    alt="A young cool bald guy in a suit" />
+                <img src={Picture}  width="100%"    alt="A young cool bald guy in a suit" />
             </div>
             <div className='abtext'>
                 <h2>Hello, I'm <font color="#b10000" >S</font>ario <font color="#b10000" >T</font>edesco.<br/></h2>
 
-                Hi, I'm Sario Tedesco! I'm a Computer Science and Telecommunications (expert).<br/>
+                Hi, I'm Sario Tedesco! I'm a Computer Science and Telecommunications expert.<br/>
                 I live in Italy, and my greatest passions are Videogames and Anime.<br/>
                 I've always connected with Computer Science, which made me explore by autodidact many types of Arts, from drawing to video editing and VFX.
                 All of these experiences helped me a lot when I chose to enter the Game Industry.<br/>
