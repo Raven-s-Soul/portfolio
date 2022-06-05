641e6c83e0a2a3a55e2a9b33240d3c733a4f914c
sario-tedesco <sariotedesco@gmail.com>
2022-06-05 22:39:33 +0200

    update

--- src/components/Design.js
+++ src/components/Design.js
@@ -46,7 +46,11 @@ class Design extends Component {
                         <div className="card-body">
                             <h5 className="card-title">Mobile Game Design Blockudoku</h5>
                             <p className="card-text">That's how I would like to change the game adding a new game mode, increasing the monetization and the retention.</p>
-                            <a href="https://docs.google.com/presentation/d/13LXzzE1bQXQWuCEluzaY3FD7iOn9y327_Cv6Gy_EHcs/edit?usp=sharing" className="btn btn-outline-danger">Go check</a>
+                            <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://docs.google.com/presentation/d/13LXzzE1bQXQWuCEluzaY3FD7iOn9y327_Cv6Gy_EHcs/edit?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
                         </div>
                     </div>
 
@@ -59,8 +63,12 @@ class Design extends Component {
                       <div className="card-body">
                           <h5 className="card-title">How I breake down FPS</h5>
                           <p className="card-text">FPS are different from past, now is hard talking about a pure fps, but they got some consistency on the evolving of the genre.</p>
-                          <a href="https://docs.google.com/document/d/1v2br63cFN1Fr27bQlx4ygebL2P3CEfKZgT-YCMOefyk/edit?usp=sharing" className="btn btn-outline-danger">Go check</a>
-                      </div>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://docs.google.com/document/d/1v2br63cFN1Fr27bQlx4ygebL2P3CEfKZgT-YCMOefyk/edit?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
+                        </div>
                   </div>
 
               </div>
@@ -72,7 +80,11 @@ class Design extends Component {
                       <div className="card-body">
                           <h5 className="card-title">RPG Weapons and Armors</h5>
                           <p className="card-text">A quick example how I would balance armors and weapons inside an RPG.</p>
-                          <a href="https://docs.google.com/spreadsheets/d/1h74pBPI8Yitim0J-WlAp3KoAd5mpNzeZqNByzFP4IbY/edit?usp=sharing" className="btn btn-outline-danger">Go check</a>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://docs.google.com/spreadsheets/d/1h74pBPI8Yitim0J-WlAp3KoAd5mpNzeZqNByzFP4IbY/edit?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
                       </div>
                   </div>
 
@@ -85,8 +97,12 @@ class Design extends Component {
                       <div className="card-body">
                           <h5 className="card-title">Making a new RTS</h5>
                           <p className="card-text">I made the concept for a new RTS that is based on use only 3 enemy.</p>
-                          <a href="https://docs.google.com/document/d/1mrueTnG4O_p_a1JosZehc33XA5HDcEEEXRGFvZsVLoY/edit?usp=sharing" className="btn btn-outline-danger">Go check</a>
-                      </div>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://docs.google.com/document/d/1mrueTnG4O_p_a1JosZehc33XA5HDcEEEXRGFvZsVLoY/edit?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
+                          </div>
                   </div>
 
               </div>
@@ -98,7 +114,11 @@ class Design extends Component {
                       <div className="card-body">
                           <h5 className="card-title">Uncertainties inside Elden Ring</h5>
                           <p className="card-text">I tried to found out all the uncertainties inside the title of FromSoftware.</p>
-                          <a href="https://docs.google.com/document/d/1ciF6XcGldduHYx7LSGxSO8yi8HkapPfHjNy-FfulqwQ/edit?usp=sharing" className="btn btn-outline-danger">Go check</a>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://docs.google.com/document/d/1ciF6XcGldduHYx7LSGxSO8yi8HkapPfHjNy-FfulqwQ/edit?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
                       </div>
                   </div>
 
@@ -111,7 +131,11 @@ class Design extends Component {
                           <h5 className="card-title">Abarei</h5>
                           <p className="card-text">That's a Unity Vigamus Academy Project. I worked as a designer and narrative of the game; the game genre is an endless runner where the Abarai seeks the collectibles to gather more power.
                             <br/>WIP</p>
-                          <a href="#..." className="btn btn-outline-danger">Go check</a>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='http://google.com';
+                              }} className="btn btn-outline-danger">Go check</button>
                       </div>
                   </div>
               </div>
--- src/components/Dev.js
+++ src/components/Dev.js
@@ -31,7 +31,11 @@ class Dev extends Component {
                             <h5 className="card-title">Alicia's Alien Adventure:<br/>Escape from GlurbGlorb!!</h5>
                             <p className="card-text"> That's my 1st ever game project, and jam was a 48h project on Unreal 4, and I worked as a Developer.<br/>
                             We made a 2-player 2D sidescroller game, where Alicia (main character) uses a high-technological assistant Matrix(her self-designed wheelchair) to escape from Glurbglorbian Empire.</p>
-                            <a href="https://gitlab.com/sario-tedesco/jam-11-21" className="btn btn-outline-danger">Go check</a>
+                            <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://gitlab.com/sario-tedesco/jam-11-21';
+                              }} className="btn btn-outline-danger">Go check</button>
                         </div>
                     </div>
 
@@ -44,8 +48,12 @@ class Dev extends Component {
                       <div className="card-body">
                           <h5 className="card-title">Aut Aut</h5>
                           <p className="card-text">That's my 2nd game jam, a 48h project on Unreal 4; I worked as a solo dev and did a bit of an artist, like doing the sprite of characters. The game concept is on dualism, so I used the level streaming to run two words and make the player free to complete platform and puzzle levels across two dimensions.</p>
-                          <a href="https://globalgamejam.org/2022/games/aut-aut-9" className="btn btn-outline-danger">Go check</a>
-                      </div>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://globalgamejam.org/2022/games/aut-aut-9';
+                              }} className="btn btn-outline-danger">Go check</button>
+                        </div>
                   </div>
 
               </div>
@@ -57,7 +65,11 @@ class Dev extends Component {
                       <div className="card-body">
                           <h5 className="card-title">PurpleBreakpoint - Last Christmas</h5>
                           <p className="card-text">That's an Unreal Vigamus Academy Project, where we had to complete the different tasks that were requested. The Concept is "Christmas in Australia"; I worked majorly as a dev and designed some areas in the game.</p>
-                          <a href="https://drive.google.com/drive/folders/1jzR-pa10WMX29AF-w733s9HhSmJCMuNx?usp=sharing" className="btn btn-outline-danger">Go check</a>
+                          <button type="button"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href='https://drive.google.com/drive/folders/1jzR-pa10WMX29AF-w733s9HhSmJCMuNx?usp=sharing';
+                              }} className="btn btn-outline-danger">Go check</button>
                       </div>
                   </div>
               </div>
