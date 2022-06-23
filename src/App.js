017f9016ba0f3f8c6c57841aad93b90a5fba47c5
sario-tedesco <sariotedesco@gmail.com>
2022-06-23 02:07:00 +0200

    BrowserRouter into HashRouter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import './App.css';
-import React from 'react'
-import About from './components/About';
-import Design from './components/Design';
-import Dev from './components/Dev';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-
-
-
+import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Design from "./components/Design";
+import Dev from "./components/Dev";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <Router>
-        <div className="App">
-          <Navbar />
-            <Routes>
-                <Route path='portfolio/about' element={<About/>}/>
-                <Route path='portfolio/design' element={<Design/>}/>
-                <Route path='portfolio/dev' element={<Dev/>}/>
-                <Route path='portfolio/' element={<Home/>}/>
-            </Routes>
-
-
-
-
-
-
-        </div>
-      </Router>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="portfolio/about" element={<About />} />
+          <Route path="portfolio/design" element={<Design />} />
+          <Route path="portfolio/dev" element={<Dev />} />
+          <Route path="portfolio/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
