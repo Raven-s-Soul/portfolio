51aa9e579c71d164945848d3844929e169a58c50
sario-tedesco <sariotedesco@gmail.com>
2022-07-06 01:20:05 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -57,6 +57,14 @@ function Footer() {
             onClick={(e) => {
               e.preventDefault();
               window.open("mailto:sariotedesco@gmail.com");
+              var input = document.createElement("input");
+              input.setAttribute("value", "sariotedesco@gmail.com");
+              document.body.appendChild(input);
+              input.select();
+              var result = document.execCommand("copy");
+              document.body.removeChild(input);
+              alert("Copied sariotedesco@gmail.com in the clipboard");
+              return result;
             }}
           >
             <img src={Gmail} loading="lazy" className="footer-Img" alt="..." />
