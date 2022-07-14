68caed1df8e65cc49f7ee8428c47dd74daab04c6
sario-tedesco <55527565+sario-tedesco@users.noreply.github.com>
2022-07-14 17:07:11 +0000

    Deploying to gh-pages from @ sario-tedesco/portfolio@557fd4e73dfe36e5554cae43d7d1cc4439065b0c 🚀

---
+++ precache-manifest.7a07718cf051b55b21cd535728c4f435.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "40be9711fc7dc9de4159",
+    "url": "/portfolio/static/css/main.21159cae.chunk.css"
+  },
+  {
+    "revision": "40be9711fc7dc9de4159",
+    "url": "/portfolio/static/js/main.40be9711.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/portfolio/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "174d5b55934d53ff047d",
+    "url": "/portfolio/static/css/2.782775ee.chunk.css"
+  },
+  {
+    "revision": "174d5b55934d53ff047d",
+    "url": "/portfolio/static/js/2.174d5b55.chunk.js"
+  },
+  {
+    "revision": "6fb5dfa365a80f80c9a7",
+    "url": "/portfolio/static/js/runtime~main.6fb5dfa3.js"
+  },
+  {
+    "revision": "25caee5786454f769aa1c7a0e19373cb",
+    "url": "/portfolio/static/media/FullLogoEmpty.25caee57.gif"
+  },
+  {
+    "revision": "36fe6deb081621e0c75b5853914d1b6c",
+    "url": "/portfolio/static/media/Blockudoku.36fe6deb.png"
+  },
+  {
+    "revision": "6566dd54528b783fea376a4547ed6b9f",
+    "url": "/portfolio/static/media/FPS.6566dd54.png"
+  },
+  {
+    "revision": "a45a2bf3c03bd87c99a758f8077e1ad9",
+    "url": "/portfolio/static/media/RPG.a45a2bf3.png"
+  },
+  {
+    "revision": "cbbbc5e628b07350b5346d7ac1fb6d56",
+    "url": "/portfolio/static/media/RTS.cbbbc5e6.png"
+  },
+  {
+    "revision": "be000cb6b74209f40b4aa6bb6d6f3143",
+    "url": "/portfolio/static/media/Uncertainties-EldenRing.be000cb6.png"
+  },
+  {
+    "revision": "d18fe9bb97d9f5a1e4402aedec4980c0",
+    "url": "/portfolio/static/media/Abarei.d18fe9bb.png"
+  },
+  {
+    "revision": "99fc86921371c5b99a06ea1f8195f47d",
+    "url": "/portfolio/static/media/Alicia_s_AlienAdventure.99fc8692.png"
+  },
+  {
+    "revision": "7f8890150389c59f6bf82e413c87decc",
+    "url": "/portfolio/static/media/AutAut.7f889015.png"
+  },
+  {
+    "revision": "fb3e6130c6ead4773adab227ed8eb1af",
+    "url": "/portfolio/static/media/PurpleBreakpoint.fb3e6130.png"
+  },
+  {
+    "revision": "be271e09960b9c064634de2b60bb5c9b",
+    "url": "/portfolio/static/media/PictureAbout.be271e09.png"
+  },
+  {
+    "revision": "e3c3400e875fc53c54dad5ca9934b4b9",
+    "url": "/portfolio/static/media/Discord.e3c3400e.png"
+  },
+  {
+    "revision": "b8494c0bbcf5e244e6b97ffe057bb03e",
+    "url": "/portfolio/static/media/github-logo.b8494c0b.png"
+  },
+  {
+    "revision": "5ff0ee9944bf430af97e50cc067f780a",
+    "url": "/portfolio/static/media/Gmail.5ff0ee99.png"
+  },
+  {
+    "revision": "6842143bc5426ec192813367349292b2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.06b723de503aa4142d115822bc15a265.js"
+  "/portfolio/precache-manifest.7a07718cf051b55b21cd535728c4f435.js"
 );
 
 workbox.clientsClaim();
