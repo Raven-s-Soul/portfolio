bad3c19fa53bfa3da70864d10953a2d038576a3c
sario-tedesco <sariotedesco@gmail.com>
2022-08-22 12:46:57 +0200

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,24 +16,24 @@ function Home() {
         />
         <div className="abtext">
           <h2>
-            <font color="#b10000 ">S</font>ario <font color="#b10000 ">T</font>
-            edesco
+            <font color="#b10000 ">R</font>aven's{" "}
+            <font color="#b10000 ">S</font>
+            oul
           </h2>
-          Hi, I'm Sario Tedesco! I'm a Computer Science and Telecommunications
+          Hi, I'm Raven's Soul! I'm a Computer Science and Telecommunications
           expert.
           <br /> I live in Italy, and my greatest passions are Videogames and
           Anime. I've always connected with Computer Science, which made me
           explore by autodidact many types of Arts, from drawing to video
-          editing and VFX. All of these experiences helped me a lot when I chose
-          to enter the Game Industry.
+          editing and VFX.
           <br />
           I'm a big fan of FPS games; I played competitively, but I chose to
           retire pretty early to focus on my studies, and when I got a bit of
           free time, I started coaching teams.
-          <br />I like free space activities, especially when with other people,
-          but what I like the most is doing great teamwork, and I like it even
-          more when everyone works perfectly with others that look like some
-          gears inside a pocket watch.
+          <br />I like free space activities and esports, especially when with
+          other people, but what I like the most is doing great teamwork, and I
+          like it even more when everyone works perfectly with others that look
+          like some gears inside a pocket watch.
         </div>
       </div>
       <div className="container2">
